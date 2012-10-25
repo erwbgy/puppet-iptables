0ecdecca813d93ff81abf3af4416b57b5a5e7aa1
Keith Burdis <keith@burdis.org>
2012-10-25 13:31:08 +0100

    Remove all allow and deny files before the main stage where iptables::allow and iptables::deny resources are added.  This ensures that ports configured in previous runs are not included.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,7 @@
 class iptables {
+  stage { 'first': before => Stage['main'] }
+  class { 'iptables::clean': stage => 'first' }
+  include iptables::clean
   include iptables::config
   include iptables::update
   include iptables::install

4dcee5d1fde463819c053b055d698de55398fb00
Keith Burdis <keith@burdis.org>
2012-10-29 08:58:39 +0000

    Read and apply configuration from hiera.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,6 @@
-class iptables {
+class iptables (
+  $use_hiera = true
+) {
   stage { 'first': before => Stage['main'] }
   class { 'iptables::clean': stage => 'first' }
   include iptables::clean
@@ -6,5 +8,32 @@ class iptables {
   include iptables::update
   include iptables::install
   include iptables::service
-  include iptables::default
+  if $use_hiera {
+    $iptables = hiera_hash('iptables')
+    if !$iptables {
+      include iptables::default
+    }
+    else {
+      $allow_tcp = $iptables['allow_tcp']
+      $allow_udp = $iptables['allow_udp']
+      $drop_tcp  = $iptables['drop_tcp']
+      $drop_udp  = $iptables['drop_udp']
+      # May not work with arrays in which case restructure YAML
+      if $allow_tcp {
+        create_resources( 'iptables::allow', $allow_tcp, { protocol => 'tcp' } )
+      }
+      if $allow_udp {
+        create_resources( 'iptables::allow', $allow_udp, { protocol => 'udp' } )
+      }
+      if $drop_tcp {
+        create_resources( 'iptables::drop',  $drop_tcp,  { protocol => 'tcp' } )
+      }
+      if $drop_udp {
+        create_resources( 'iptables::drop',  $drop_udp,  { protocol => 'udp' } )
+      }
+    }
+  }
+  else {
+    include iptables::default
+  }
 }
